b8585e786a781368794c84e50e6143e8d6d5e319
lqqyt2423 <974923609@qq.com>
2020-04-15 18:14:14 +0800

    add test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,11 @@
 import * as path from 'path';
 import * as os from 'os';
 import * as crypto from 'crypto';
+import * as mkdirp from 'mkdirp';
+
+export async function initMkdirp() {
+  await mkdirp(getPath('html/asset'));
+}
 
 export function getPath(pathname: string): string {
   return path.join(os.homedir(), '.we-art-deskapp', pathname);

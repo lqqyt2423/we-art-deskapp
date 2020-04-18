d34c5fb8c2abe7132f19e9d804ab650d31898cb3
lqqyt2423 <974923609@qq.com>
2020-04-18 14:05:26 +0800

    change ad url

--- src/utils/ad.ts
+++ src/utils/ad.ts
@@ -1,11 +1,9 @@
-// https://raw.githubusercontent.com/lqqyt2423/we-art-deskapp/master/message.json
-
 import * as rp from 'request-promise';
 
 const version = require('../../package.json').version as string;
 
 export async function get(): Promise<string> {
-  let endpoint = 'http://localhost:2423/api/we-art-desk/ad';
+  let endpoint = 'https://shuke.applinzi.com/api/we-art-desk/ad';
   endpoint += '?version=' + version;
   const data = await rp.get(endpoint, { json: true });
 
